d4e6dc9eacf0817ed84b07372c3434c8ad6976a3
Daksh Singh <daksh.singh.2407@gmail.com>
2025-12-19 21:59:37 +0530

    Implement PncSolver class with methods for permutations and combinations, and update Main class to demonstrate usage. - the methods are pretty much self explanatory in the added class pncsolver :)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import pnc.base.*;
+import pnc.solver.PncSolver;
 
 public class Main {
     public static void main(String[] args) {
-        Combination perm = new Combination(5);
-        System.out.println("Number of combinations of C(5,2): " + perm.combineWithVariableValue(2));
-        System.out.println("Number of combinations of C(5,3): " + perm.combineWithVariableValue(3));
-        System.out.println("Number of combinations of C(5,2): " + perm.getCombination());
+        PncSolver.getHelp();
+        PncSolver.getExamplesOfMethods();
+        PncSolver pnc = new PncSolver(5, "combination");
+        long result = pnc.calculateCombination(2);
+        System.out.println("Combination Result: " + result);
     }
 }
---
+++ src/pnc/solver/PncSolver.java
@@ -0,0 +1,72 @@
+package pnc.solver;
+
+public class PncSolver {
+    int totalobjectsN;
+    String method;
+
+    public PncSolver(int totalobjectsN, String method, String title) {
+        this.totalobjectsN = totalobjectsN;
+        if (method.toLowerCase() == "permutation") this.method = "permutation";
+        else if (method.toLowerCase() == "combination") this.method = "combination";
+        else this.method = "unknown";
+    }
+
+    public PncSolver(int totalobjectsN, String method) {
+        this.totalobjectsN = totalobjectsN;
+        if (method.toLowerCase() == "permutation") this.method = "permutation";
+        else if (method.toLowerCase() == "combination") this.method = "combination";
+        else this.method = "unknown";
+    }
+
+    public int getTotalObjectsN() { return totalobjectsN; }
+    public String getMethod() { return method; }
+
+    public static void getHelp() {
+        System.out.println("Available methods:\n" +
+                "You can provide title (that's completely optional, just for better understanding) at the last parameter of constructor.\n" +
+                "1. permutation - Calculate permutations of N objects.\n" +
+                "2. combination - Calculate combinations of N objects.\n" +
+                "Usage:\n" +
+                "Create an instance of PncSolver with totalobjectsN and method.\n" +
+                "Then call the appropriate calculation method based on the chosen method.");
+    }
+
+    public static void getHelp(String method) {
+        if (method.toLowerCase() == "permutation") {
+            System.out.println("Permutation Method Help:\n" +
+                    "Calculate permutations of N objects.\n" +
+                    "Usage:\n" +
+                    "Create an instance of PncSolver with totalobjectsN and 'permutation' as method.\n" +
+                    "Then call calculatePermutation() to get the result.");
+        } else if (method.toLowerCase() == "combination") {
+            System.out.println("Combination Method Help:\n" +
+                    "Calculate combinations of N objects.\n" +
+                    "Usage:\n" +
+                    "Create an instance of PncSolver with totalobjectsN and 'combination' as method.\n" +
+                    "Then call calculateCombination(waystoselectC) to get the result.");
+        } else {
+            System.out.println("Unknown method. Available methods are 'permutation' and 'combination'.");
+        }
+    }
+
+    public static void getExamplesOfMethods() {
+        // Real life examples can be added here. Like, selecting team members, arranging books.
+        System.out.println("Examples of Methods:\n" +
+                "1. Permutation: Arranging 5 books on a shelf.\n" +
+                "2. Combination: Selecting 3 team members from a group of 5.");
+    }
+
+    public long calculatePermutation() {
+        if (this.method == "permutation") {
+            int factorialN = (new pnc.base.Factorial(this.totalobjectsN)).getFactorial();
+            return factorialN;
+        } else return -1;
+    }
+
+    public long calculateCombination(int waystoselectC) {
+        if (this.method == "combination") {
+            pnc.base.Combination combinationCalculator = new pnc.base.Combination(this.totalobjectsN, waystoselectC);
+            return combinationCalculator.getCombination();
+        } else return -1;
+    }
+}
